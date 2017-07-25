5ceb253d26a47fed964d79182f88a2c5ff04298a
dcchivian <dcchivian@lbl.gov>
2017-07-25 12:50:41 -0700

    dev

--- test/kb_assembly_compare_server_test.py
+++ test/kb_assembly_compare_server_test.py
@@ -87,3 +87,40 @@ class kb_assembly_compareTest(unittest.TestCase):
         # Check returned data with
         # self.assertEqual(ret[...], ...) or other unittest methods
         pass
+    # NOTE: According to Python unittest naming rules test method names should start from 'test'. # noqa
+    def test_run_benchmark_assemblies_against_genomes_with_MUMmer4_test_01 (self):
+        # Prepare test objects in workspace if needed using
+        # self.getWsClient().save_objects({'workspace': self.getWsName(),
+        #                                  'objects': []})
+        #
+        # Run your method by
+        # ret = self.getImpl().your_method(self.getContext(), parameters...)
+        #
+        # Check returned data with
+        # self.assertEqual(ret[...], ...) or other unittest methods
+
+        # input_data
+        reference_prok_genomes_WS = 'ReferenceDataManager'  # PROD and CI
+        #reference_prok_genomes_WS = '19217'  # PROD
+        #reference_prok_genomes_WS = '15792'  # CI
+
+        genome_ref_1 = reference_prok_genomes_WS+'/GCF_001566335.1/1'  # E. coli K-12 MG1655
+        genome_ref_2 = reference_prok_genomes_WS+'/GCF_000021385.1/1'  # D. vulgaris str. 'Miyazaki F'
+        genome_ref_3 = reference_prok_genomes_WS+'/GCF_000006805.1/1'  # Halobacterium salinarum NRC-1 (3 contigs)
+
+        sister_genome_ref_1 = reference_prok_genomes_WS+'/GCF_000357445.2/1'  # Escherichia coli P0298942.3 (207 contigs)
+        sister_genome_ref_2 = reference_prok_genomes_WS+'/GCF_000195755.1/1'  # DvH (2 contigs)
+        sister_genome_ref_3 = reference_prok_genomes_WS+'/GCF_000069025.1/1'  # Halobacterium salinarum R1 (5 contigs)
+        sister_assembly_ref_1 = reference_prok_genomes_WS+'/442/1'
+        sister_assembly_ref_2 = reference_prok_genomes_WS+'/48316/1'
+        sister_assembly_ref_3 = reference_prok_genomes_WS+'/194909/1'
+
+        parameters = { 'workspace_name': self.getWsName(),
+                       'desc': 'test assembly benchmark',
+                       'input_genome_refs': [genome_ref_1, genome_ref_2, genome_ref_3],
+                       'input_assembly_refs': [sister_assembly_ref_1, sister_assembly_ref_2, sister_assembly_ref_3]
+                     }
+
+        ret = self.getImpl().run_benchmark_assemblies_against_genomes_with_MUMmer4(self.getContext(), parameters)
+
+        pass
